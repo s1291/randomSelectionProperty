2da2e4f2444ff120178de00aad1b5c88f3ccc7b7
samir1291 <samirmath01@gmail.com>
2022-01-20 23:12:26 +0100

    Refactor and fix some issues with transformations

--- random_selection.py
+++ random_selection.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-'''
+"""
 Copyright (C) 2021-2022 Samir OUCHENE, samirmath01@gmail.com
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,25 +14,45 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301, USA.
-'''
+"""
 
 import random
 import inkex
-from lxml import etree
-from inkex import TextElement
+
 
 class CRandSel(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
-        
-        self.arg_parser.add_argument('-p', "--property", action='store',
-                                    type=str, dest="property", default='width',
-                                    help='Property to modify')
-       
-        self.arg_parser.add_argument('-n', '--value', action='store', type=float, dest='value',
-                default='10', help='property value')
-        self.arg_parser.add_argument('-k', '--howMany', action='store', type=int, dest='howMany',
-                default=1, help='How many object to select randomly')
+
+        self.arg_parser.add_argument(
+            "-p",
+            "--property",
+            action="store",
+            type=str,
+            dest="property",
+            default="width",
+            help="Property to modify",
+        )
+
+        self.arg_parser.add_argument(
+            "-n",
+            "--value",
+            action="store",
+            type=float,
+            dest="value",
+            default="10",
+            help="property value",
+        )
+        self.arg_parser.add_argument(
+            "-k",
+            "--howMany",
+            action="store",
+            type=int,
+            dest="howMany",
+            default=1,
+            help="How many object to select randomly",
+        )
+
     def effect(self):
 
         objects = self.svg.selection
@@ -45,9 +65,8 @@ class CRandSel(inkex.Effect):
 
         rand_objs = [objects[i] for i in rand_sel]
 
-
         for obj in rand_objs:
-            #self.msg(f"{list(obj.bounding_box())}")
+            # self.msg(f"{list(obj.bounding_box())}")
             x_x = list(obj.bounding_box().x)
             y_y = list(obj.bounding_box().y)
             x, y = float(x_x[0]), float(y_y[0])
@@ -60,22 +79,24 @@ class CRandSel(inkex.Effect):
                 scalex = _value / orig_val
                 scaley = 1
 
-                targetx = (1 - scalex)*x
+                targetx = (1 - scalex) * x
                 targety = y
 
-                obj.set("transform", f"scale({scalex} {scaley}) translate({targetx/scalex} 0)")
+                obj.transform.add_scale(scalex, scaley)
+                obj.transform.add_translate(targetx / scalex, 0)
 
             elif _property.lower() == "height":
                 orig_val = curr_height
                 scalex = 1
                 scaley = _value / orig_val
                 targetx = x
-                targety = (1-scaley)*y
+                targety = (1 - scaley) * y
 
-                obj.set("transform", f"scale({scalex} {scaley}) translate(0 {targety/scaley})")
+                obj.transform.add_scale(scalex, scaley)
+                obj.transform.add_translate(0, targety / scaley)
+
             else:
                 self.errormsg(f"property '{_property}' Not implemented yet.")
-
 
         self.svg.update()
 
